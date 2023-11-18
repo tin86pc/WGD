c895d1932a2f2152e8c6d1b4a0302b9bbb189d45
tu m4800 <tinpc07@gmail.com>
2023-11-18 13:37:37 +0700

    18.11

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # WGD
+
+1. npm init
+
+2. npm install express --save
+
+3. npm install dotenv --save
+
+4. npm install ejs
+
+5. npm install body-parser
+
+6. npm i nodemon
+
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+// require("dotenv").config()
+
+import { } from 'dotenv/config'
+
+import configViewEngine from './configs/viewEngine.js'
+
+import initWebRoutes from './routes/web.js'
+
+const app = express()
+configViewEngine(app)
+initWebRoutes(app)
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log('http://localhost:' + PORT);
+})
